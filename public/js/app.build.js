644c0d8daa78c377111aafb5639b6dd272dcf035
Damien Angelos <bostaunieux@gmail.com>
2012-11-22 12:37:26 -0500

    optimization working

---
+++ public/js/app.build.js
@@ -0,0 +1,34 @@
+({
+	mainConfigFile: './main.js',
+    paths: {
+        'jquery':      'vendor/jquery/jquery-1.8.3',
+        'jquery-ui':   'vendor/jquery/jquery-ui-1.8.23.custom.min',
+        'underscore':  'vendor/underscore/underscore-1.3.3',
+        'backbone':    'vendor/backbone/backbone-0.9.2',
+        'bootstrap':   'vendor/bootstrap/bootstrap',
+        'tablesorter': 'vendor/jquery/jquery.tablesorter',
+        'tablesorter-widgets': 'vendor/jquery/jquery.tablesorter.widgets',
+        'domReady':    'vendor/require/domReady-2.0.1'
+    },
+    appDir: "../../",
+    baseUrl: "public/js",
+    dir: "../../../fenrir2-build",
+    modules: [
+		{
+			name: "main",
+			include: [ 'jquery', 'jquery-ui', 'underscore', 'backbone', 'domReady!', 'config' ]
+	   	},
+		{
+			name: "pages/index",
+			exclude: [ "main" ]
+		},
+		{
+			name: "pages/lift",
+			exclude: [ "main" ]
+		},
+		{
+			name: "pages/prefs",
+			exclude: [ "main" ]
+		}
+    ]
+})
--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,9 @@ require.config({
 	baseUrl: '/js/',
 
     paths: {
-        'jquery':      'vendor/jquery/jquery-1.8.0.min',
+        'jquery':      'vendor/jquery/jquery-1.8.3',
         'jquery-ui':   'vendor/jquery/jquery-ui-1.8.23.custom.min',
-        'underscore':  'vendor/underscore/underscore-1.3.3',
+        'underscore':  'vendor/underscore/underscore-1.4.2',
         'backbone':    'vendor/backbone/backbone-0.9.2',
         'bootstrap':   'vendor/bootstrap/bootstrap',
         'tablesorter': 'vendor/jquery/jquery.tablesorter',
