d3e95eee721650098a983668900fd085cc1b7d5d
Damien Angelos <bostaunieux@gmail.com>
2012-12-17 20:19:04 -0500

    updating underscore version; css tweak

--- public/js/app.build.js
+++ public/js/app.build.js
@@ -3,7 +3,7 @@
     paths: {
         'jquery':      'vendor/jquery/jquery-1.8.3',
         'jquery-ui':   'vendor/jquery/jquery-ui-1.8.23.custom.min',
-        'underscore':  'vendor/underscore/underscore-1.3.3',
+        'underscore':  'vendor/underscore/underscore-1.4.2',
         'backbone':    'vendor/backbone/backbone-0.9.2',
         'bootstrap':   'vendor/bootstrap/bootstrap',
         'tablesorter': 'vendor/jquery/jquery.tablesorter',
